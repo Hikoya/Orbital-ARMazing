b866c7cf4f077d3b0ab3bf2e2e82a2663d673c2e
Hikoya <hikoyanamikaze@gmail.com>
2022-05-18 17:07:25 +0800

    Initialised basic database and installed Chakra-UI for theme
    Initialized new PlanetScale database and linked Prisma ORM to the database.
    
    Installed ChakraUI component library for styling and configured

---
+++ Web/src/pages/_app.js
@@ -0,0 +1,15 @@
+import { SessionProvider } from "next-auth/react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <ChakraProvider>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ChakraProvider>
+  );
+}
