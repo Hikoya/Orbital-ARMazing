30ffd65caaa99c5cd32736d0b6cb73081be0dc72
MinHeinA <minheinaung97@gmail.com>
2022-05-23 21:26:00 +0800

    Updated AR UI element & wrote AR Manager script
    - Beautified AR UI by adding color and borders
    - Wrote AR Manager script that updates the text UI elements of AR UI panel according to the name of tracked image

---
+++ Unity/ARmazing/Assets/_PROJECT/Script/ARManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using TMPro;
+
+[RequireComponent (typeof (ARTrackedImageManager))]
+
+public class ARManager : MonoBehaviour
+{
+    private ARTrackedImageManager _trackedImageManager;
+
+    [SerializeField] private GameObject _arUICanvas;
+    [SerializeField] private TMP_Text tileText;
+    [SerializeField] private TMP_Text descText;
+
+    private void Awake()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        _trackedImageManager = GetComponent<ARTrackedImageManager>();
+        _arUICanvas.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        _trackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    private void OnDisable()
+    {
+        _trackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach(ARTrackedImage trackedImage in eventArgs.added)
+        {
+            UpdateARUI(trackedImage);
+        }
+
+        foreach(ARTrackedImage trackedImage in eventArgs.updated)
+        {
+            if (trackedImage.trackingState == TrackingState.Tracking)
+            {
+                UpdateARUI(trackedImage);
+            } 
+            else
+            {
+                _arUICanvas.SetActive(false);
+            }
+        }
+    }
+
+    void UpdateARUI(ARTrackedImage trackedImage)
+    {
+        _arUICanvas.SetActive(true);
+        tileText.text = trackedImage.referenceImage.name;
+        descText.text = "Description of " + trackedImage.referenceImage.name;
+    }
+}
