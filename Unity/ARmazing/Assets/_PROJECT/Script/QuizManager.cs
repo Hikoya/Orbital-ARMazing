d47c07a28f3219bee878a8540a04a7132eb8b8f3
MinHeinA <minheinaung97@gmail.com>
2022-05-28 11:59:15 +0800

    Completed quiz behaviour for Milestone 1
    - Completed quiz behaviour for milestone 1
    - UI react correctly to choice, if chosen answer is correct it turns green and red when wrong

---
+++ Unity/ARmazing/Assets/_PROJECT/Script/QuizManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class QuizManager : MonoBehaviour
+{
+    public List<QuestionAndAnswers> QnA;
+    public GameObject[] options;
+    public int currentQuestion;
+
+    public TMP_Text QuestionTxt;
+
+    private void Start()
+    {
+        GenerateQuestion();
+    }
+
+    public void NextQuestion()
+    {
+        QnA.RemoveAt(currentQuestion);
+        GenerateQuestion();
+        EnableOptions();
+    }
+
+    public void DisableOptions()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<Button>().interactable = false;
+        }
+    }
+
+    public void EnableOptions()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<Button>().interactable = true;
+        }
+    }
+
+    void SetAnswers()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<AnswerButtonBehaviour>().isCorrect = false;
+            options[i].transform.GetChild(0).GetComponent<TMP_Text>().text = QnA[currentQuestion].Answers[i];
+
+            if(QnA[currentQuestion].CorrectAnswer == i + 1)
+            {
+                options[i].GetComponent<AnswerButtonBehaviour>().isCorrect = true;
+            }
+        }
+    }
+
+    void GenerateQuestion()
+    {
+        currentQuestion = Random.Range(0, QnA.Count);
+
+        QuestionTxt.text = QnA[currentQuestion].Question;
+        SetAnswers();
+    }
+}
