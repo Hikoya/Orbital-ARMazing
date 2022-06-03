2db6d0f79f49748b6eb56042f2b6216cac08a5e3
Hikoya <hikoyanamikaze@gmail.com>
2022-06-04 02:44:19 +0800

    Summary Statistics
    Show number of assets, events and users that joined an event created

--- Web/src/pages/index.js
+++ Web/src/pages/index.js
@@ -1,15 +1,100 @@
 import { cardVariant, parentVariant } from "@root/motion";
 import { motion } from "framer-motion";
-import { Box, SimpleGrid } from "@chakra-ui/react";
+import {
+  Box,
+  SimpleGrid,
+  Stack,
+  Stat,
+  StatLabel,
+  StatNumber,
+  Text,
+} from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 import Card from "@components/Card";
 import Auth from "@components/Auth";
 const MotionSimpleGrid = motion(SimpleGrid);
 const MotionBox = motion(Box);
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+
+  const [event, setEvent] = useState(0);
+  const [user, setUser] = useState(0);
+  const [asset, setAsset] = useState(0);
+
+  const generateStatistic = async (content) => {
+    if (content.event) {
+      setEvent(content.event);
+    }
+
+    if (content.user) {
+      setUser(content.user);
+    }
+
+    if (content.asset) {
+      setAsset(content.asset);
+    }
+  };
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const rawResponse = await fetch("/api/dashboard/statistic", {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+      const content = await rawResponse.json();
+      if (content.status) {
+        await generateStatistic(content.msg);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    async function generate() {
+      await fetchData();
+    }
+
+    generate();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Auth>
       <Box>
+        <Box bg="white" borderRadius="lg" p={8} color="gray.700" shadow="base">
+          <Stack direction={"horizontal"}>
+            {!loading && (
+              <>
+                <Stat>
+                  <StatLabel>Total Events</StatLabel>
+                  <StatNumber>{event}</StatNumber>
+                </Stat>
+                <Stat>
+                  <StatLabel>Total Assets</StatLabel>
+                  <StatNumber>{asset}</StatNumber>
+                </Stat>
+                <Stat>
+                  <StatLabel>Total Users joined</StatLabel>
+                  <StatNumber>{user}</StatNumber>
+                </Stat>
+              </>
+            )}
+
+            {loading && (
+              <Box align="center" justify="center" mt={30}>
+                <Text>Loading Please wait...</Text>
+              </Box>
+            )}
+          </Stack>
+        </Box>
+
         <MotionSimpleGrid
           mt="3"
           minChildWidth={{ base: "full", md: "500px", lg: "500px" }}
