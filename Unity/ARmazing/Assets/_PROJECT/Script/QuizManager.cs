e89c0fb1c4f53e34b5d7c1b520b5c07b9c5ca481
MinHeinA <minheinaung97@gmail.com>
2022-06-16 22:12:59 +0800

    Create Quiz using JSON data
    - Implemented script to create Quiz question and answers using JSON data
    - Imported JSON phaser from Unity asset store to replace built-in that has limited functionality (https://assetstore.unity.com/packages/tools/input-management/json-net-for-unity-11347#publisher)
    - To be implemented in the future: research and ensure code works with android file system

--- Unity/ARmazing/Assets/_PROJECT/Script/QuizManager.cs
+++ Unity/ARmazing/Assets/_PROJECT/Script/QuizManager.cs
@@ -5,17 +5,22 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.ARFoundation;
+using System.IO;
+using Newtonsoft.Json;
 
 public class QuizManager : MonoBehaviour
 {
     public List<QuestionAndAnswers> QnA;
     public GameObject[] options;
     public int currentQuestion;
+    string filename;
 
     public TMP_Text QuestionTxt;
 
     private void Start()
     {
+        filename = "testquiz.json";
+        QnA = ReadQuizFromJSON(filename);
         GenerateQuestion();
     }
 
@@ -49,12 +54,16 @@ public class QuizManager : MonoBehaviour
 
     void SetAnswers()
     {
+        options[0].transform.GetChild(0).GetComponent<TMP_Text>().text = QnA[currentQuestion].option1;
+        options[1].transform.GetChild(0).GetComponent<TMP_Text>().text = QnA[currentQuestion].option2;
+        options[2].transform.GetChild(0).GetComponent<TMP_Text>().text = QnA[currentQuestion].option3;
+        options[3].transform.GetChild(0).GetComponent<TMP_Text>().text = QnA[currentQuestion].option4;
+
         for (int i = 0; i < options.Length; i++)
         {
             options[i].GetComponent<AnswerButtonBehaviour>().isCorrect = false;
-            options[i].transform.GetChild(0).GetComponent<TMP_Text>().text = QnA[currentQuestion].Answers[i];
 
-            if(QnA[currentQuestion].CorrectAnswer == i + 1)
+            if(QnA[currentQuestion].answer == i + 1)
             {
                 options[i].GetComponent<AnswerButtonBehaviour>().isCorrect = true;
             }
@@ -65,7 +74,66 @@ public class QuizManager : MonoBehaviour
     {
         currentQuestion = Random.Range(0, QnA.Count);
 
-        QuestionTxt.text = QnA[currentQuestion].Question;
+        QuestionTxt.text = QnA[currentQuestion].question;
         SetAnswers();
     }
+
+    List <QuestionAndAnswers> ReadQuizFromJSON(string filename)
+    {
+        string content = ReadFile(GetPath(filename));
+        Debug.Log(content);
+        if (string.IsNullOrEmpty(content) || content == "{}")
+        {
+            return null;
+        }
+
+        QuizJSONObj res = JsonConvert.DeserializeObject<QuizJSONObj>(content);
+        Debug.Log("Status: " + res.status);
+        Debug.Log("Error: " + res.error);
+        Debug.Log("Msg: " + res.msg);
+        return res.msg;
+    }
+
+    string GetPath(string filename)
+    {
+        return "Assets/_PROJECT/Data/JSON/" + filename;
+    }
+
+    string ReadFile(string path)
+    {
+        if(File.Exists(path))
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string content = reader.ReadToEnd();
+                return content;
+            }
+        }
+        return "";
+    }
+    
+}
+
+public class QuizJSONObj
+{
+    public bool status;
+    public string error;
+    public List<QuestionAndAnswers> msg;
+}
+
+public class QuestionAndAnswers
+{
+    public string id;
+    public string eventName;
+    public string asset;
+    public string eventID;
+    public string assetID;
+    public string question;
+    public string option1;
+    public string option2;
+    public string option3;
+    public string option4;
+    public int answer;
+    public int points;
+    public bool visible;
 }
